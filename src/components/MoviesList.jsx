d9018eee57a8f451686d82462d5188142e0c039b
Bulgarsky <bulgarpepper@gmail.com>
2023-12-24 18:37:24 +0500

    added ENV local

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,17 +1,20 @@
 import Movie from "./Movie.jsx";
 
 function MoviesList(props){
-
-    const {movies} = props;
+    //нач.массив пустой
+    const {movies =[] } = props;
 
     return(
         <div className="moviesList">
-            {movies.map(movie => (
-                <Movie
-                    key={movie.imdbID}
-                    {...movie}
-                />
-            ))
+            {
+                movies.length
+                    //если ничего не найдено апи возвращает Error, для этого нач.массив задан пустым
+                    ? (movies.map(movie => (
+                        <Movie
+                            key={movie.imdbID}
+                            {...movie}
+                        />)))
+                    :  <h3>Ничего не найдено ! </h3>
             }
         </div>
     )
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,34 +3,44 @@ import MoviesList from "../components/MoviesList.jsx";
 import Preloader from "../components/Preloader.jsx";
 import {Search} from "../components/Search.jsx";
 
+
+//use your api key for OMBDapi.com
+//env local
+const OMDB_API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+
+
 class Main extends React.Component{
     state= {
         movies: [],
+        loading: true,
     }
 
+
     componentDidMount(){
-        fetch("http://www.omdbapi.com/?apikey=8dc5c654&s=matrix")
+        fetch(`http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=christmas`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search}));
+            .then(data => this.setState({movies: data.Search, loading: false}));
     }
 
     searchMovie = (word, type = "all") => {
-        fetch(`http://www.omdbapi.com/?apikey=8dc5c654&s=${word}${type !=="all" ? `&type=${type}` : ""}`)
+        this.setState({ loading: true });
+        fetch(`http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${word}${type !== "all" ? `&type=${type}` : ""}`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search}));
+            .then(data => this.setState({movies: data.Search, loading: false}));
     }
 
 
     render(){
-        const {movies} = this.state;
+        const {movies, loading} = this.state;
 
         return(
             <div className="container content">
                 <Search searchMovie={this.searchMovie}/>
                 {
-                    movies.length
-                        ? <MoviesList movies={this.state.movies}/>
-                        : <Preloader />
+                    // movies.length
+                    loading
+                        ? <Preloader />
+                        : <MoviesList movies={movies}/>
                 }
 
             </div>
